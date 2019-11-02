97f59b2bc7124ab7c669e5d2530d8b6d1db46c39
xinyun-c <xinyun_c@berkeley.edu>
2019-11-02 12:51:25 -0700

    10/30 work by Xinyun Cao

--- DLT_numbers/Assets/Scripts/EventManagerDichoticListening.cs
+++ DLT_numbers/Assets/Scripts/EventManagerDichoticListening.cs
@@ -7,20 +7,34 @@ public class EventManagerDichoticListening : MonoBehaviour
     public AudioSource IntroAudio;
     public OVRCameraRig Player;
     public GameObject difficultyManager;
-    public GameObject game;
+    public GameObject speaker1;
+    public GameObject speaker2;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(PlayAudio(IntroAudio));
-        //supposedly wait for player to set difficulty here
-        game.SetActive(true);
+        StartCoroutine(Intro());
+    }
 
-    }
-    IEnumerator PlayAudio(AudioSource audioSource)
+    IEnumerator Intro()
     {
-        audioSource.Play();
-        yield return new WaitForSeconds(audioSource.clip.length); // wait time
+        IntroAudio.Play();
+        yield return new WaitForSeconds(IntroAudio.clip.length); // wait time
+        //supposedly wait for player to set difficulty here
+        selectTarget();
+        speaker1.SetActive(true);
+        speaker2.SetActive(true);
     }
+
+    private void selectDifficulty() {
+    }
+
+    private void selectTarget() {
+        System.Random rnd = new System.Random();
+        int target = rnd.Next(1, 21);  // creates a number between 1 and 20
+        SpeakerTarget script1 = speaker1.GetComponent<SpeakerTarget>();
+        script1.target = target;
+    }
+
     // Update is called once per frame
     void Update()
     {
---
+++ DLT_numbers/Assets/Scripts/SpeakerTarget.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakerTarget : MonoBehaviour
+{
+    public int target;
+    public int last_num;
+    public GameObject numberAudio;
+    private List<AudioSource> audioList;
+    private bool responded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        audioList = numberAudio.GetComponent<NumberAudio>().audioList;
+        for (int i = 1; i <= 25; i++)
+        {
+            responded = false;
+            SelectRandomNumber();
+            StartCoroutine(audioPlay(audioList[last_num]));
+
+        }
+    }
+
+    IEnumerator audioPlay(AudioSource num_audio) {
+        num_audio.Play();
+        yield return new WaitForSeconds(num_audio.clip.length + 1); // wait audioclip length time plus one second
+    }
+
+    private void SelectRandomNumber() {
+        float probTarget = Random.Range(0.0f, 1.0f);
+        if (probTarget < 0.3)
+        {
+            last_num = target;
+        }
+        else {
+            System.Random rnd = new System.Random();
+            last_num = rnd.Next(1, 21);  // creates a number between 1 and 20
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
